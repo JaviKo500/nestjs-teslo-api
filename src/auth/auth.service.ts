e353124951aee49a7d94eb031414766b132a5cbb
JaViKo500 <javikogutierrez64@gmail.com>
2024-08-25 10:08:53 -0500

    register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+  private readonly logger = new Logger('AuthService');
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {
+    
+  }
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create( createUserDto );
+      await this.userRepository.save( user );
+      return user;
+    } catch (error) {
+      this.handelDBExceptions(error)
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  private handelDBExceptions(error: any): never {
+    if ( error.code === '23505') 
+      throw new BadRequestException(error.detail);
+    
+    if ( error.status === 404 ) 
+      throw new NotFoundException( error.message );
+    
+    this.logger.error( `${error.message} - ${error.detail}` );
+    throw new InternalServerErrorException('Unexpected error check server logs');
   }
 }
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -24,7 +24,7 @@ export class User {
 
    @Column({
       type: 'bool',
-      unique: true,
+      default: true,
    })
    public isActive: boolean;
 
