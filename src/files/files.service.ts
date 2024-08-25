c580c8399a1f6d59642978d9de838a369521dc40
JaViKo500 <javikogutierrez64@gmail.com>
2024-08-25 08:33:59 -0500

    get images

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
 
 @Injectable()
 export class FilesService {
-  create(createFileDto: any) {
-    return 'This action adds a new file';
+  getStaticImage(type: string, imageName: string) {
+    const path = join( __dirname, `../../static/${type}`, imageName);
+    if ( !existsSync( path ) ) {
+      throw new BadRequestException(`Not found image /${ type }/${ imageName }`);
+    }
+    return path;
   }
 }
