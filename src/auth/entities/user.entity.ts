be77200727f9624a05ecec30eca6b5e0a9e15109
JaViKo500 <javikogutierrez64@gmail.com>
2024-08-25 09:50:51 -0500

    user entity

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+
+   @PrimaryGeneratedColumn('uuid')
+   public id: string;
+
+   @Column({
+      type: 'text',
+      unique: true,
+   })
+   public email: string;
+   
+   @Column({
+      type: 'text',
+   })
+   public password: string;
+
+   @Column({
+      type: 'text',
+   })
+   public fullName: string;
+
+   @Column({
+      type: 'bool',
+      unique: true,
+   })
+   public isActive: boolean;
+
+   @Column({
+      type: 'text',
+      array: true,
+      default: [ 'user' ]
+   })
+   public roles: string[];
+}
