22b9a11d26af031c07c23937afa843341fa95ee3
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-01 10:35:26 -0500

    login user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import * as bcrypt from 'bcrypt';
 
-import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
-
+import { CreateUserDto, LoginUserDto } from './dto';
 @Injectable()
 export class AuthService {
 
@@ -16,16 +15,16 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {
-    
+
   }
   async create(createUserDto: CreateUserDto) {
     try {
       const { password, ...userData } = createUserDto;
-      const user = this.userRepository.create( {
+      const user = this.userRepository.create({
         ...userData,
-        password: bcrypt.hashSync( password, 10  )
+        password: bcrypt.hashSync(password, 10)
       });
-      await this.userRepository.save( user );
+      await this.userRepository.save(user);
       delete user.password;
       return user;
     } catch (error) {
@@ -33,14 +32,29 @@ export class AuthService {
     }
   }
 
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: {
+        email: true,
+        password: true,
+      }
+    });
+    if ( !user || !bcrypt.compareSync( password, user.password) ) 
+      throw new UnauthorizedException( 'Invalid credentials');
+    
+    return user;
+  }
+
   private handelDBExceptions(error: any): never {
-    if ( error.code === '23505') 
+    if (error.code === '23505')
       throw new BadRequestException(error.detail);
-    
-    if ( error.status === 404 ) 
-      throw new NotFoundException( error.message );
-    
-    this.logger.error( `${error.message} - ${error.detail}` );
+
+    if (error.status === 404)
+      throw new NotFoundException(error.message);
+
+    this.logger.error(`${error.message} - ${error.detail}`);
     throw new InternalServerErrorException('Unexpected error check server logs');
   }
 }
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -14,6 +14,7 @@ export class User {
    
    @Column({
       type: 'text',
+      select: false,
    })
    public password: string;
 
