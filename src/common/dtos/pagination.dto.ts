0c39f7effb8f7106c5a61e5e6a73b99b3bba5b2a
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-07 12:16:21 -0500

    Doc dtos

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,13 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDto {
 
+   @ApiProperty({
+      default: 10,
+      description: 'how many items per page'
+   })
    @IsOptional()
    @IsPositive()
    @Type( ()=> Number ) // enabledExplicitConversion
    limit?: number;
    
+   @ApiProperty({
+      default: 0,
+      description: 'How many items per page skip'
+   })
    @IsPositive()
    @IsOptional()
    @Min(0)
